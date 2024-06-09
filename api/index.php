cf9053151fe99490213b33ebe8a5689ed562cf13
SkyBula <89245764+VerfiedGameDev@users.noreply.github.com>
2024-06-09 10:31:19 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,47 +1,36 @@
 <?php
-$username = filter_input(INPUT_POST, 'username');
-$password = filter_input(INPUT_POST, 'password');
+$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+
 if (!empty($username)) {
-if (!empty($password)) {
-$host = "127.0.0.1";
-$dbusername = "root";
-$dbpassword = "";
-$dbname = "youtube";
+    if (!empty($password)) {
+        $host = "127.0.0.1";
+        $dbusername = "root";
+        $dbpassword = "";
+        $dbname = "youtube";
 
-// create connection
+        // Create connection
+        $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
 
-$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
-
-if (mysqli_connect_error()){
-    die('Connect Error('.mysqli_connect_errno() .') '
-    . mysqli_connect_error());
-}
-else{
-    $sql = "INSERT INTO account (username, password)
-    values ('$username','$password')";
-    if ($conn->query($sql)){
-        echo "New record is inserted Successfully";
+        if ($conn->connect_error) {
+            die('Connect Error: ' . $conn->connect_error);
+        } else {
+            $sql = "INSERT INTO account (username, password) VALUES ('".$conn->real_escape_string($username)."', '".$conn->real_escape_string($password)."')";
+            
+            if ($conn->query($sql)) {
+                echo "New record is inserted successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+            
+            $conn->close();
+        }
+    } else {
+        echo "Password should not be empty";
+        die();
     }
-    else{
-        echo "Error: ". $sql ."<br>". $conn->error;
-    }
-    $conn->close();
-}
-} 
-else{
-    echo "Number Should not be Empty";
+} else {
+    echo "Username should not be empty";
     die();
 }
-}
-else{
-    echo "Username Should not be empty";
-    die();
-}
-
-
-
-
-
-
-
 ?>
